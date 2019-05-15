47d0ce2aa3c3cb723a9f6859e98154c494023858
bui.thi.thuy.dung <FRAMGIA\bui.thi.thuy.dung@b121536-lt.framgia.com>
2019-05-15 11:05:11 +0700

    CRUD Api

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var todoRouter = require('./routes/todo');
+var db = require('./db/db');
 
 var app = express();
 
@@ -18,14 +21,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended:false });
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.get('/api/v1/todos', todoRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
 // error handler
 app.use(function(err, req, res, next) {
